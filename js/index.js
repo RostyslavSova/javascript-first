b31274dc2805d7d7eecdbe353bbfee9a8f6220f7
Rostyslav Sova <sova.rostislav@gmail.com>
2018-02-26 17:04:16 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// alert('Hello world');
+// document.write('Hello JS');
+// console.log('Hello JS');
+// let productTitle = 'Mazda RX8';
+// let productPrice = 40000 +20;
+// let productAvailable = true;
+// document.write('Product Price: '+productTitle+'<br>'+'Product Price: '+productPrice+'<br>'+'Product Available: '+productAvailable);
+// document.write('<br>'+'New price:'+ productPrice * 0.9);
+// let productColor;//undefiled
+// document.write('<br>'+ productColor);
+// productColor = null;
+// document.write('<br>'+ productColor);
+
+// let obj={};
+
+// console.log(Math.sqrt(4));
+// console.log(Math.sqrt(-4));
+// console.log(-4/0);
+
+// const userName = prompt('Enter your name');
+// const userAge = parseFloat(prompt('Enter your age'));
+// console.log('Will be: ' + userAge);
+// if (userAge >= 18) {
+//     console.log('Adult');
+// } else {
+//     console.log('Child');
+// }
+
+const someResult = Addd(10,20,30);
+console.log(someResult);
+
+function Add(a, b){
+    return a+b;
+}
+
+function Addd(a,b,c){
+    return a+b+c;
+}
